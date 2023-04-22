1f9ce1011ed8b696b49687dea1e820f84573c12c
Troy Hunt <github@troyhunt.com>
2023-04-22 12:19:52 +1000

    Added additional tests related to casing

--- test/Program.cs
+++ test/Program.cs
@@ -15,5 +15,34 @@ public class MyAddressExtractorTest
 
         // Assert
         Assert.IsTrue(result.Count == 12, "Parsing should pass");
+    }  
+    
+    [TestMethod]
+    public void email_addresses_are_not_case_sensitive()
+    {
+        // Arrange
+        var sut = new AddressExtractor();
+        const string addresses = "test@example.com TEST@EXAMPLE.COM";
+
+        // Act
+        var result = sut.ExtractAddresses(addresses);
+
+        // Assert
+        Assert.IsTrue(result.Count == 1, "Same addresses of different case should be merged");
+    }   
+    
+    [TestMethod]
+    public void email_addresses_are_converted_to_lowercase()
+    {
+        // Arrange
+        var sut = new AddressExtractor();
+        const string input = "TEST@EXAMPLE.COM";
+        const string expected = "test@example.com";
+
+        // Act
+        var result = sut.ExtractAddresses(input).First();
+
+        // Assert
+        Assert.AreEqual(expected, result, "Address should always be converted to lowercase");
     }
 }
