c074369c7b5dde7a0d8ed9574049e7009c373299
7873930+GStefanowich <7873930+GStefanowich@users.noreply.github.com>
2023-04-30 01:50:43 -0400

    Allow BaseFilters to update the EmailAddress value

--- src/Objects/EmailAddress.cs
+++ src/Objects/EmailAddress.cs
@@ -4,12 +4,34 @@ namespace MyAddressExtractor.Objects {
     public struct EmailAddress {
         private readonly Match Match;
 
-        public string Full => this._Full ??= this.Match.Value;
+        /// <summary>
+        /// <para>The full email address.</para>
+        /// 
+        /// This value can be set by <see cref="AddressFilter.BaseFilter"/>s, to trim or otherwise make tweaks to the address.
+        /// <b>Be careful changing this</b>, filters are not rerun. The new value may not have passed previous filters.
+        /// </summary>
+        /// <example>username@test.com</example>
+        public string Full {
+            get => this._Full ??= this.Match.Value;
+            set {
+                // Update the full address to something else
+                this._Full = value ?? throw new NullReferenceException();
+
+                // Clear the cache
+                this._Separator = null;
+                this._Username = null;
+                this._Domain = null;
+            }
+        }
         private string? _Full = null;
 
+        /// <summary>The username part of the address.</summary>
+        /// <example>username</example>
         public string Username => this._Username ??= this.Full[..this.Separator];
         private string? _Username = null;
 
+        /// <summary>The domain part of the address.</summary>
+        /// <example>test.com</example>
         public string Domain => this._Domain ??= this.Full[this.Separator..];
         private string? _Domain = null;
 
@@ -17,7 +39,8 @@ namespace MyAddressExtractor.Objects {
         private int Separator => this._Separator ??= this.Full.LastIndexOf('@');
         private int? _Separator = null;
 
-        public int Length => this.Match.Length - this.Match.Index;
+        /// <summary>The Length of the Full Address</summary>
+        public int Length => this._Full?.Length ?? this.Match.Length - this.Match.Index;
 
         public EmailAddress(Match match)
         {
