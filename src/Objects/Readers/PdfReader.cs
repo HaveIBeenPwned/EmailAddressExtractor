6c4e548190e5fcee67fce7fb255756f4a1c011bd
Fredrik Haarstad <swift.year6073@zomg.no>
2026-04-25 22:19:03 +0200

    Implemented PdfReader using PdfPig to extract text from PDF files

--- src/Objects/Readers/PdfReader.cs
+++ src/Objects/Readers/PdfReader.cs
@@ -1,15 +1,26 @@
+using System.Runtime.CompilerServices;
 using HaveIBeenPwned.AddressExtractor.Objects.Attributes;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.TextExtractor;
 
 namespace HaveIBeenPwned.AddressExtractor.Objects.Readers;
 
 [ExtensionTypes(".pdf")]
-public sealed class PdfReader : ILineReader
+internal sealed class PdfReader(string path) : ILineReader
 {
     /// <inheritdoc />
-    public IAsyncEnumerable<string?> ReadLineAsync(CancellationToken cancellation = default)
-        => throw new NotImplementedException();
+    public async IAsyncEnumerable<string?> ReadLineAsync([EnumeratorCancellation] CancellationToken cancellation = default)
+    {
+        using var document = PdfDocument.Open(path);
+        foreach (var page in document.GetPages())
+        {
+            cancellation.ThrowIfCancellationRequested();
+            var text = ContentOrderTextExtractor.GetText(page);
+            await Task.Yield();
+            yield return text;
+        }
+    }
 
     /// <inheritdoc />
-    public ValueTask DisposeAsync()
-        => ValueTask.CompletedTask;
+    public ValueTask DisposeAsync() => ValueTask.CompletedTask;
 }
